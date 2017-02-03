22417467a3531a788cdd54a549dae6f9e5e52b67
bbord22 <bmucle@yahoo.com>
2017-02-03 05:06:31 -0600

    Began experimenting with physics

---
+++ Assets/circleBumperForce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class circleBumperForce : MonoBehaviour
+{
+
+	public Rigidbody2D rb;
+	public Collision2D collN;
+	public ForceMode2D thrust;
+	public Vector2 force;
+
+
+	public void start()
+	{
+		rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+		rb = GetComponent<Rigidbody2D> ();
+		thrust = ForceMode2D.Impulse;
+		collN = GetComponent<Collision2D> ();
+
+	}
+
+	public void OnCollisionEnter2D(Collision2D collN)
+	{
+		if (collN.gameObject.tag == "Circular Bumper") 
+		{
+			force.x = 0;
+			force.y = 3;
+			rb.AddRelativeForce (force, thrust);
+		}
+	}
+		
+}
