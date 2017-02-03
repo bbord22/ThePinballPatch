a3d485c1f317c22619f7604a52ceb7883e6d33ea
bbord22 <bmucle@yahoo.com>
2017-02-03 13:53:42 -0600

    Finished circular bumper physics
    Will need to tweak force output in the future.

--- Assets/BouncerLogic.cs
+++ Assets/BouncerLogic.cs
@@ -9,7 +9,8 @@ public class BouncerLogic : MonoBehaviour
 	public Rigidbody2D rb;
 	public Collision2D collN;
 	public ForceMode2D thrust;
-	public Vector2 force;
+	public float force;
+	Vector2 dir;
 
 
 	public void start()
@@ -17,21 +18,29 @@ public class BouncerLogic : MonoBehaviour
 		rb = GetComponent<Rigidbody2D> ();
 		thrust = ForceMode2D.Impulse;
 		collN = GetComponent<Collision2D> ();
-
 	}
 
 	public void OnCollisionEnter2D(Collision2D collN)
 	{
 		if (collN.gameObject.tag == "Circular Bumper") 
 		{
-			force.x = 0;
-			force.y = 5;
-			rb.AddRelativeForce (force, thrust);
+			force = 5;
+			float x1 = collN.contacts [0].point.x;
+			float y1 = collN.contacts[0].point.y;
+			float x2 = transform.position.x;
+			float y2 = transform.position.y;
+			dir.x = x1 - x2;
+			dir.y = y1 - y2;
+			dir = -dir.normalized;
+			rb.AddRelativeForce (dir*force, thrust);
 		}
 
 		/*else if (collN.gameObject.tag == "LaunchSpring") 
 		{
-			
+			if()
+			{
+				
+			}
 		}*/
 	}
 
