61ca359d38d776754071257f2ee59487f8500a39
bbord22 <bmucle@yahoo.com>
2017-02-05 01:44:44 -0600

    Started script to control flippers

---
+++ Assets/FlipperLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperLogic : MonoBehaviour 
+{
+	public HingeJoint2D hinge;
+	public float reference;
+
+	void Start () 
+	{
+		hinge = gameObject.GetComponent<HingeJoint2D> ();
+		reference = 213.02f;
+		hinge.referenceAngle = 213.02;
+	}
+	
+
+	void Update () {
+		
+	}
+}
