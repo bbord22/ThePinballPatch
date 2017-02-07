7c7d8fb013b7087f2d7c166adaf80ea0f38928f3
bbord22 <bmucl2@yahoo.com>
2017-02-06 18:46:26 -0600

    Added music to game and an easter egg

---
+++ Assets/EasterEggLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggLogic : MonoBehaviour {
+
+	public AudioClip sandstorm;
+	public AudioSource mainplayer;
+	public AudioSource otherplayer;
+
+	void Start () 
+	{
+		mainplayer = GameObject.Find ("MusicObj").GetComponent<AudioSource>();
+		otherplayer = gameObject.GetComponent<AudioSource> ();	
+		DontDestroyOnLoad (gameObject);
+
+		if (FindObjectsOfType (GetType ()).Length > 1) 
+		{
+			Destroy (gameObject);
+		} 
+	}
+	
+	void OnTriggerEnter2D()
+	{
+		if(!(otherplayer.isPlaying))
+		{
+			sandstorm = Resources.Load <AudioClip> ("AudioFiles/sandstorm");
+			GameObject.Find ("MusicObj").SetActive (false);
+			otherplayer.GetComponent<AudioSource> ().clip = sandstorm;
+			otherplayer.GetComponent<AudioSource> ().Play ();
+			otherplayer.GetComponent<AudioSource> ().volume = .8f;
+		}
+	}
+}
---
+++ Assets/musicPersistenceLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicPersistenceLogic : MonoBehaviour 
+{
+	public AudioSource player;
+
+	void Start () 
+	{
+		DontDestroyOnLoad (gameObject);	
+
+		if ((FindObjectsOfType (GetType ()).Length > 1) || GameObject.Find("EasterEggTrigger").GetComponent<AudioSource>().isPlaying) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
