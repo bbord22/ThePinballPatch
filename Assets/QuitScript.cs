0df723080459ff56d346cec359e37c6ab54fff56
bbord22 <bmucle@yahoo.com>
2017-02-07 11:54:19 -0600

    Finished Project w/ Executable ZIP

---
+++ Assets/QuitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour {
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Application.Quit ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			SceneManager.LoadScene ("Game");
+		}
+	}
+}
